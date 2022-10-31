33f5621e77326eea8dde9d5593533867e4a8452f
kamalkumar7 <202051098@iiitvadodara.ac.in>
2022-10-31 18:40:11 +0530

    calendar updated
    by brijesh

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
+import ContextWrapper from './pages/calendar/context/ContextWrapper'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
+    
+  <AuthContextProvider>
+
+  <ContextWrapper>
+  <App />
+  </ ContextWrapper>
+  
     </AuthContextProvider>
   </React.StrictMode>
+  
 );
